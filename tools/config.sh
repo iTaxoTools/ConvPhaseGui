e99efbb2e16f0930cfa227268a21d755031124fd
StefanPatman <stefanpatman91@gmail.com>
2023-12-11 20:09:32 +0200

    Update building tools

--- tools/config.sh
+++ tools/config.sh
@@ -5,21 +5,28 @@
 
 # program specifics
 APP_NAME="ConvPhase"
-APP_IDENTIFIER="org.itaxotools.convphase"
-APP_SCRIPT="../convphase.py"
+APP_IDENTIFIER="org.itaxotools.convphase.gui"
+APP_SCRIPT="run.py"
 APP_ENTITLEMENTS="data/entitlements.plist"
-APP_ICON="data/convphase.icns"
-
+APP_ICON_ICNS="data/convphase.icns"
+APP_ICON_ICO="data/convphase.ico"
 
 # expand and export
 export CODESIGN_IDENTITY=$CODESIGN_IDENTITY
 export APP_NAME="$APP_NAME"
 export APP_IDENTIFIER="$APP_IDENTIFIER"
 
-DIR=$(readlink -f $(dirname $0))
+export APP_FILENAME="$APP_NAME"
+export APP_SUFFIX=$(IFS=-; echo "$*")
+if [ -n "$APP_SUFFIX" ]; then
+    export APP_FILENAME="$APP_NAME-$APP_SUFFIX"
+fi
+
+DIR="$(cd "$(dirname "$0")" && pwd)"
 export APP_SCRIPT="$DIR/$APP_SCRIPT"
 export APP_ENTITLEMENTS="$DIR/$APP_ENTITLEMENTS"
-export APP_ICON="$DIR/$APP_ICON"
+export APP_ICON_ICNS="$DIR/$APP_ICON_ICNS"
+export APP_ICON_ICO="$DIR/$APP_ICON_ICO"
 
 export APP_BUNDLE="$PWD/dist/$APP_NAME.app"
-export APP_IMAGE="$PWD/dist/$APP_NAME.dmg"
+export APP_IMAGE="$PWD/dist/$APP_FILENAME.dmg"
--- tools/run.py
+++ tools/run.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"""Launch the Taxi2 GUI"""
+"""Launch the ConvPhase GUI"""
 
 import multiprocessing
 
